a837cfa71678d97289e801dbcd0a4260ac7e0e34
Mesut Öz <135958508+MesutBJK3531@users.noreply.github.com>
2023-12-02 14:13:43 +0300

    Uçak savaş oyunu(Kodlar)

--- Form1.cs
+++ Form1.cs
@@ -7,86 +7,166 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Oyun_Denemesi
+namespace Savaş_Uçağı_Oyunu
 {
     public partial class Form1 : Form
     {
-        int boruhizi = 8;
-        int gravity = 10;
-        int score = 0;
 
+        int solhareket = 0;
+        int ucakhareketihizi =10;
+        Random rnd = new Random();
+        int mermihizi = 25;
+        bool ates = false;
+        int puan = 0;
+      
+      
 
         public Form1()
         {
-            
             InitializeComponent();
+            pbDusmanucak1.Top = -500;
+            pbDusmanucak2.Top = -900;
+            pbDusmanucak3.Top = -1300;
+            pbMermi.Top = -100;
+            pbMermi.Left = -100;
         }
 
-        private void gameTimerEvent(object sender, EventArgs e)
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            
-            pbKartal.Top += gravity;
-
-            pbPipeAlt.Left -= boruhizi; 
-            pbPipeUst.Left -= boruhizi;
-            lblScore.Text="SCORE"+score;
-
-            if (pbPipeAlt.Left < -150)
+            if (e.KeyCode == Keys.Enter)
             {
-                pbPipeAlt.Left = 800;
-                score++;
+                timer1.Start();
+            }
+            if (e.KeyCode == Keys.P)
+            {
+                timer1.Stop();
             }
 
-            if (pbPipeUst.Left < -180)
+            if (e.KeyCode == Keys.Left)
             {
-                pbPipeUst.Left = 950;
+                if (pbUcak.Location.X < 0) { solhareket = 0; } else { solhareket = -10; }
             }
-
-            if (pbKartal.Bounds.IntersectsWith(pbPipeAlt.Bounds) || pbKartal.Bounds.IntersectsWith(pbPipeUst.Bounds) || pbKartal.Bounds.IntersectsWith(pbZemin.Bounds))
+            else if (e.KeyCode == Keys.Right)
             {
-                //Çarpışma
-                oyunbittimi();
-                
-                if (score > 5)
+                if (pbUcak.Location.X > 512)
                 {
-                    boruhizi = 15;
+                    solhareket = 0;
                 }
-                if (pbKartal.Top < -25)
+                else
                 {
-                    oyunbittimi();
+                    solhareket = 10;
+                }
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+
+                if (ates == false)
+                {
+                    mermihizi = 8;
+                    pbMermi.Left = pbUcak.Left + 40;
+                    pbMermi.Top = pbUcak.Top;
+                    ates = true;
                 }
             }
 
-            if (gravity < 10)
-                gravity++;
         }
-         
 
-        private void gamekeyisdown(object sender, KeyEventArgs e)
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Space)
+            if (e.KeyCode == Keys.Left)
             {
-                gravity = -10;
-                
+                solhareket = 0;
             }
-            else if(e.KeyCode == Keys.Return)
+            else if (e.KeyCode == Keys.Right)
             {
-                gameTimer.Start();
+                solhareket = 0;
             }
         }
-
-        private void gamekeyisup(object sender, KeyEventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            if (e.KeyCode == Keys.Space)
+            pbUcak.Left += solhareket;
+            pbMermi.Top -= mermihizi;
+            pbDusmanucak1.Top += ucakhareketihizi;
+            pbDusmanucak2.Top += ucakhareketihizi;
+            pbDusmanucak3.Top += ucakhareketihizi;
+            lblSkor.Text = "" + puan;
+      
+                if (pbUcak.Bounds.IntersectsWith(pbDusmanucak1.Bounds) || (pbUcak.Bounds.IntersectsWith(pbDusmanucak2.Bounds) ||( pbUcak.Bounds.IntersectsWith(pbDusmanucak3.Bounds))))
             {
-                gravity = 1;
+                oyunSonu();
+
             }
+            if (ates && pbMermi.Top < 0)
+            {
+                ates = false;
+                mermihizi = 0;
+                pbMermi.Top = -100;
+                pbMermi.Left = -100;
+            }
+            Vurulma();
         }
 
-           private void oyunbittimi()
-          {
-               gameTimer.Stop();
-               lblScore.Text="GAME OVER!!!";
-          }
+        private void oyunSonu()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show(puan + " Puan Kazandınız....");
+            puan = 0;
+            lblSkor.Text = "0";
+            pbDusmanucak1.Top = -500;
+            pbDusmanucak2.Top = -900;
+            pbDusmanucak3.Top = -1300;
+            pbMermi.Top = -100;
+            pbMermi.Left = -100;
+            timer1.Enabled = true;
+        }
+
+        private void Vurulma()
+        {
+            if (pbMermi.Bounds.IntersectsWith(pbDusmanucak1.Bounds))
+            {
+                puan += 1;
+                pbDusmanucak1.Top = -500;
+                int ranP = rnd.Next(1, 300);
+                pbDusmanucak1.Left = ranP;
+                ates = false;
+                mermihizi = 0;
+                pbMermi.Top = -100;
+                pbMermi.Left = -100;
+
+
+            }
+            else if (pbMermi.Bounds.IntersectsWith(pbDusmanucak2.Bounds))
+            {
+                puan += 1;
+                pbDusmanucak2.Top = -900;
+                int ranP = rnd.Next(1, 400);
+                pbDusmanucak2.Left = ranP;
+                ates = false;
+                mermihizi = 0;
+                pbMermi.Top = -100;
+                pbMermi.Left = -100;
+
+            }
+            else if (pbMermi.Bounds.IntersectsWith(pbDusmanucak3.Bounds))
+            {
+                puan += 1;
+                pbDusmanucak3.Top = -1300;
+                int ranP = rnd.Next(1, 500);
+                pbDusmanucak3.Left = ranP;
+                ates = false;
+                mermihizi = 0;
+                pbMermi.Top = -100;
+                pbMermi.Left = -100;
+
+            }
+
+        }
     }
 }
+
+
+
+      
+
